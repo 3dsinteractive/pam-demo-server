3b10b937a87da2da4d2887f5af548f4f9d0d68fd
pong3ds <chaiyapong@3dsinteractive.com>
2021-03-29 14:38:49 +0700

    Read timestamp from cmd

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+TIMESTAMP=$1
+
+if [ "$TIMESTAMP" = "" ]; then
+    echo -e "TIMESTAMP cannot be blank"
+    exit 1
+fi
+
 GO=/usr/local/go/bin/go
 if [ -f "$GO" ]; then
     /usr/local/go/bin/go get
@@ -9,6 +16,5 @@ else
     go mod vendor
 fi
 
-TIMESTAMP=$(date +"%Y%m%d%H%M%S")
 docker build -f Dockerfile -t 3dsinteractive/pam-demo-server:$TIMESTAMP .
 docker push 3dsinteractive/pam-demo-server:$TIMESTAMP
